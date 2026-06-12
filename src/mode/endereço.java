e28d500c628ddece9d6b46fa7eed5a7ee34fc521
jk-jhon1 <jk709649@gmail.com>
2026-06-12 20:43:34 -0300

    Add Endereco class with properties and methods

--- src/mode/endereço.java
+++ src/mode/endereço.java
@@ -1,1 +1,29 @@
+package model;
 
+public class Endereco {
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String cep;
+
+    public Endereco(String rua, String numero, String bairro, String cidade, String cep) {
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+    }
+
+    // Getters
+    public String getRua() { return rua; }
+    public String getNumero() { return numero; }
+    public String getBairro() { return bairro; }
+    public String getCidade() { return cidade; }
+    public String getCep() { return cep; }
+
+    @Override
+    public String toString() {
+        return rua + ", " + numero + " - " + bairro + ", " + cidade + " (CEP: " + cep + ")";
+    }
+}
